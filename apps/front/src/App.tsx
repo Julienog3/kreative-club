f8db5fa487897ebe2dde3327ce6b3655a7e4fb67
Julien Auger <julien.auger37@laposte.net>
2023-10-25 21:02:55 +0200

    feat(storybook): Installing Storybook

--- apps/front/src/App.tsx
+++ apps/front/src/App.tsx
@@ -1,32 +1,20 @@
 import { center, vstack } from '../styled-system/patterns'
 import { css } from '../styled-system/css';
-import UsersPage from './pages/UsersPage';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Card from './components/utils/Card/Card';
 
 const queryClient = new QueryClient()
 
 function App() {
-
-
-
   return (
     <QueryClientProvider client={queryClient}>
       <main className={center({ height: '100vh', width: 'full', backgroundColor: '#F9F5F2' })}>
-        <div 
-          className={css({ 
-            border: '3px solid #000', 
-            boxShadow: '4px 4px 0px #000', 
-            borderRadius: '13px',
-            bgColor: '#fff',
-            padding: 8
-          })}
-        >
+        <Card>
           <div className={vstack({ gap: 0 })}>
             <h1 className={css({ textStyle: 'title' })}>Kreative club</h1>
             <p className={css({ textStyle: 'body' })}>Plateforme de freelance pour graphiste</p>
           </div>
-          <UsersPage />
-        </div>
+        </Card>
       </main>
     </QueryClientProvider>
   )
