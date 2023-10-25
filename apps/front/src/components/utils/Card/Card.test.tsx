fa78d7fc82e17acd559e2cee2f06aabd6133ba36
Julien Auger <julien.auger37@laposte.net>
2023-10-25 21:14:28 +0200

    feat(test): Adding vitest

---
+++ apps/front/src/components/utils/Card/Card.test.tsx
@@ -0,0 +1,11 @@
+import { describe, test, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import Card from './Card';
+
+describe('Card test', () => {
+  test('Should display content', () => {
+    render(<Card><p>Hello world !</p></Card>);
+
+    expect(screen.getByText(/Hello world !/i)).toBeDefined();
+  })
+})
