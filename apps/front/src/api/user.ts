46ff29d50f85f98d1d74225db9f348cb4bb87e6a
Julien Auger <jauger@amiltone.com>
2023-10-31 15:06:08 +0100

    fixing script ssh

--- apps/front/src/api/user.ts
+++ apps/front/src/api/user.ts
@@ -12,13 +12,15 @@ export type UserPayload = User & {
 };
 
 const getUsers = async (): Promise<User[]> => {
-  return await fetch("http://localhost:3333/users").then((res) => res.json());
+  return await fetch(`${import.meta.env.VITE_API_URL}/users`).then((res) =>
+    res.json(),
+  );
 };
 
 const createUser = async (user: UserPayload): Promise<void> => {
   const userPayload = parseCamelToSnakeCase(user);
 
-  await fetch("http://localhost:3333/users", {
+  await fetch(`${import.meta.env.VITE_API_URL}/users`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
